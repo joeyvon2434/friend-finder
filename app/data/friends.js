5788c0eb49514ef9acdb99acd00508ec813a6fe5
Joseph V <joseph.vonedwins@yahoo.com>
2018-08-13 15:13:43 -0400

    completed routing to post a new friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,11 +4,39 @@ var data = {};
 data.friendList = [
     {
         name: "A Bagel",
-        imageLink: "",
+        imageLink: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Bagel-Plain-Alt.jpg/1200px-Bagel-Plain-Alt.jpg",
         scores: [
             5, 4, 2, 2, 2, 4, 3, 4, 3, 5
         ]
+    },
+    {
+        name: "Morty Smith",
+        imageLink: "https://vignette.wikia.nocookie.net/rickandmorty/images/4/41/Morty_Smith.jpg/revision/latest?cb=20170217193441",
+        scores: [
+            4, 3, 2, 5, 3, 4, 1, 4, 3, 5
+        ]
+    },
+    {
+        name: "A Family of Sheep",
+        imageLink: "http://www.animalspot.net/wp-content/uploads/2018/03/Pictures-of-Farm-Animals.jpg",
+        scores: [
+            4, 5, 2, 3, 4, 2, 3, 3, 2, 5
+        ]
+    },
+    {
+        name: "A Family of Sheep",
+        imageLink: "http://www.animalspot.net/wp-content/uploads/2018/03/Pictures-of-Farm-Animals.jpg",
+        scores: [
+            4, 5, 2, 3, 4, 2, 3, 3, 2, 5
+        ]
+    },{
+        name: "A Magical Unicorn",
+        imageLink: "https://previews.123rf.com/images/sivanova/sivanova1709/sivanova170900014/86959615-cute-magical-unicorn-vector-design-on-white-background-print-for-t-shirt-or-sticker-romantic-hand-dr.jpg",
+        scores: [
+            2, 3, 4, 2, 1, 5, 4, 4, 1, 3
+        ]
     }
+    
 ];
 
 module.exports = data;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,7 @@
 //dependencies
 var fs = require('fs');
 var path = require('path');
-var friendList = require('./../data/friends.js');
+var friends = require('./../data/friends.js');
 
 //export routes
 module.exports = function(app) {
@@ -10,9 +10,14 @@ module.exports = function(app) {
     var apiRoutes = require('express').Router();
 
     apiRoutes.get('/friendlist', function(req, res) {
-        res.json(friendList);
+        res.json(friends);
         //res.send('api test');
-        
+    });
+
+    apiRoutes.post("/friendlist", function(req, res) {
+        var newFriend = req.body;
+        friends.friendList.push(newFriend);
+        console.log(friends);
     });
 
     return apiRoutes;
